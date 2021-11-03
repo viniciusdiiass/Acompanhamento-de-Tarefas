6c32d371bbfeb94abb9394d169cc6b438224b760
Vinicius Dias <vinicius100.silva@gmail.com>
2021-11-03 10:48:48 -0300

    adicionando data/hora em nossas tarefas

---
+++ main.js
@@ -0,0 +1,44 @@
+import BotaoConclui from "./componentes/concluiTarefas.js";
+import BotaoDeleta from "./componentes/deletaTarefa.js";
+
+
+const handleNovoItem = (evento) => {
+    event.preventDefault();
+    const lista = document.querySelector('[data-list]')
+    const input = document.querySelector('[data-form-input]');
+
+    const calendario = document.querySelector('[data-form-date]');
+    const data = moment(calendario.value);
+
+    const dataFormatada = data.format('DD/MM/YYYY')
+    const valor = input.value;
+    const dados ={
+        valor,
+        dataFormatada
+    };
+
+    const criaTarefaComData = criaTarefa(dados)
+    lista.appendChild(criaTarefaComData);
+    input.value = " "
+}
+
+const criaTarefa = ({valor, dataFormatada}) => {
+
+    
+
+    const tarefa = document.createElement('li');
+    tarefa.classList.add('task');
+    const conteudo = `<p class="content"> ${dataFormatada} * ${valor}</p>`;
+
+    tarefa.innerHTML = conteudo;
+
+    tarefa.appendChild(BotaoConclui());
+    tarefa.appendChild(BotaoDeleta())
+
+    return tarefa;
+    
+}
+
+const tarefa = document.querySelector('[data-form-button]');
+
+tarefa.addEventListener('click', handleNovoItem);
